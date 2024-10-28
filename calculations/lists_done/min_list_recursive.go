d96650472bd6cd1813a62e4593f25a574aadc629
Niko205 <niggo1187@gmail.com>
2024-10-28 18:52:38 +0100

    done forms and lists

--- calculations/lists_done/min_list_recursive.go
+++ calculations/lists_done/min_list_recursive.go
@@ -4,8 +4,18 @@ package lists
 // Wenn die Liste leer ist, wird 0 zurückgegeben.
 // ZUSATZBEDINGUNG: Diese Funktion darf keine Schleife verwenden.
 func MinListRecursive(nums []int) int {
-	// TODO
-	return 0
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	if nums[0] < nums[1] { // Vergleicht die ersten beiden Zahlen
+		nums[1] = nums[0] // Setzt die kleinere Zahl an die zweite Stelle
+	}
+	return MinListRecursive(nums[1:]) // Entfernt das erste Element und ruft die Funktion erneut auf
+	// Wiederholt die Funktion so lange bis nur noch ein Element in der Liste ist
+
 }
 
 // REMARKS
